162177f740abca1bf936d2d13c83dfd768f19b5b
Michael Bishop <cleverca22@gmail.com>
2019-02-13 21:37:16 -0400

    fix the purity issues

--- default.nix
+++ default.nix
@@ -9,10 +9,11 @@ rec {
       cp -vir flow-files $out/
       cat > $out/bin/icarus-backend-service <<EOF
       #!${stdenv.shell}
+      cd $out
       exec ${nodejs}/bin/node $out/flow-files/index.js
       EOF
       chmod +x $out/bin/icarus-backend-service
-      cp -rv package.json config $out/
+      cp -rv package.json config src tls-files $out/
     '';
     packageJson = ./package.json;
     yarnLock = ./yarn.lock;
