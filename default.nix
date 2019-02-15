fca8a15ab90f342180f33a403f169cf136cfb52c
Samuel Leathers <samuel.leathers@iohk.io>
2019-02-15 11:45:48 -0500

    use NODE_PATH so config is found outside of nix store

--- default.nix
+++ default.nix
@@ -9,11 +9,11 @@ rec {
       cp -vir flow-files $out/
       cat > $out/bin/icarus-backend-service <<EOF
       #!${stdenv.shell}
-      cd $out
+      export NODE_PATH=$out/node_modules
       exec ${nodejs}/bin/node $out/flow-files/index.js
       EOF
       chmod +x $out/bin/icarus-backend-service
-      cp -rv package.json config src tls-files $out/
+      cp -rv package.json $out/
     '';
     packageJson = ./package.json;
     yarnLock = ./yarn.lock;
